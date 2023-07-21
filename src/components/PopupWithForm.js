84f93da69bbdf3323b04f7e00459f106a9efa65b
vldan <vlad.danilov.020212@gmail.com>
2023-07-21 14:04:17 +0300

    fix: включить дефолтную валидацию форм

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -2,7 +2,7 @@ function PopupWithForm({ title, name, children, isOpen, onClose, textBtnSave, on
     return (
         <section className={`pop-up pop-up_show_${name} ${isOpen ? 'pop-up_opened' : ''}`}>
             <div className="pop-up__container">
-                <form className="pop-up__form" name={name} noValidate onSubmit={onSubmit}>
+                <form className="pop-up__form" name={name} onSubmit={onSubmit}>
                     <h2 className="pop-up__form-title">{title}</h2>
                     {children}
                     <button className="pop-up__form-button-save" type="submit" onClick={onClose}>{textBtnSave}</button>
